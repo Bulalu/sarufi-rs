6515506212eb23e7fb30e47440bc006204b3e0b2
Bulalu <elishabulalu031@gmail.com>
2023-05-02 13:22:02 +0300

    updated error and initialization of the struct ☄️

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,52 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use reqwest::{Client, ClientBuilder, header::HeaderMap, Response};
+
+pub use errors::ApiError;
+use dotenv::dotenv;
+use std::env;
+
+mod errors;
+mod utils;
+
+
+/// API struct. Exposes function to interact with the Sarufi API 🥷
+pub struct SarufiAPI {
+    client: Client,
 }
+
+impl SarufiAPI {
+    /// Creates a new instance of SarufiAPI using the provided api key
+    /// this function panics if the api_key is empty 🤒
+    pub fn new<S: Into<String>>(api_key: S) -> Result<SarufiAPI, ApiError> {
+        let owned_key = api_key.into();
+
+        utils::validate_keys(&owned_key)?;
+
+        let mut default_headers = HeaderMap::new();
+        default_headers.insert("Authorization", format!("Bearer {}", owned_key).parse().unwrap());
+        default_headers.insert("Content-Type", "application/json".parse().unwrap());
+
+
+        let client = ClientBuilder::new().default_headers(default_headers).build()?;
+
+        Ok(SarufiAPI { client })
+    }
+
+    /// Returns a list of all bots created by the user
+    pub fn get_bots(&self) -> Result<Response, ApiError> {
+        let url = utils::api_url("/bots");
+        let resp = self.client.get(&url).send()?;
+        Ok(resp)
+    }
+}
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_api_url() {
+        assert_eq!(utils::api_url("/chatbot"), "https://developers.sarufi.io/chatbot");
     }
 }
