f65d3560028aa5c0b85347acc06072780b12d504
Bulalu <elishabulalu031@gmail.com>
2023-05-02 12:28:10 +0300

    added errors 😼

---
+++ src/errors.rs
@@ -0,0 +1,36 @@
+use failure::Fail;
+pub use failure::Error;
+
+/// All possible error returned from this SDK defined as variants of this enum.
+/// 
+/// This also derives the failure::Fail trait, so it should be easier to handle and extend
+/// in clients that also support this failure crate.
+#[derive(Debug, Fail)]
+pub enum ApiError {
+  
+  #[fail(display = "Invalid api_key")]
+  InvalidApiKey(),
+ 
+  /// A generic error with message on a possible failure while interacting with the api
+  #[fail(display = "Error: {}", _0)]
+  GenericError(String),
+}
+
+impl From<reqwest::Error> for ApiError {
+  fn from(req_err: reqwest::Error) -> ApiError {
+    ApiError::GenericError(format!("{}", req_err))
+  }
+}
+
+impl From<std::io::Error> for ApiError {
+  fn from(io_err: std::io::Error) -> ApiError {
+    ApiError::GenericError(format!("{}", io_err))
+  }
+}
+
+impl From<walkdir::Error> for ApiError {
+  fn from(io_err: walkdir::Error) -> ApiError {
+    ApiError::GenericError(format!("{}", io_err))
+  }
+}
+
